f78224a345b81518748988c8978c74da0ce99902
PatataMaxtex <mathis.bueckmann@gmail.com>
2022-08-28 16:59:17 +0200

    initial commit

---
+++ src/main/java/de/mathisundmara/vegantips/VeganTipsApplication.java
@@ -0,0 +1,13 @@
+package de.mathisundmara.vegantips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeganTipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeganTipsApplication.class, args);
+	}
+
+}
